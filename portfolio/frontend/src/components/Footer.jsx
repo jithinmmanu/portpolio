f4d4091c02327ff6953add893d13ebac5a3fcaef
Jithin M <jithinmmanu309@gmail.com>
2026-03-30 17:16:12 +0530

    portfolio

--- portfolio/frontend/src/components/Footer.jsx
+++ portfolio/frontend/src/components/Footer.jsx
@@ -10,8 +10,12 @@ const Footer = () => {
         <motion.div
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
+
+
           viewport={{ once: true }}
           className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 mb-10"
+
+          
         >
           
           {/* LEFT */}
